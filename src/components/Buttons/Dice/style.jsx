7f782179f2b96387985b30929676d6bb491b28cf
remi59800 <remimenart@gmail.com>
2022-06-20 12:55:10 +0200

    fixing images bugs new

--- src/components/Buttons/Dice/style.jsx
+++ src/components/Buttons/Dice/style.jsx
@@ -1,15 +1,15 @@
 import styled from "styled-components";
 
 export default styled.div`
-    display: flex;
-    justify-content: center;
-    margin-top: 7vh;
-
-  @media screen and (max-width: 700px) {
   display: flex;
   justify-content: center;
-  margin-top: 0;
-}
+  margin-top: 7vh;
+
+  @media screen and (max-width: 700px) {
+    display: flex;
+    justify-content: center;
+    margin-top: 0;
+  }
 
   button {
     width: 8vh;
@@ -26,11 +26,22 @@ export default styled.div`
     display: inline-block;
     animation: wiggle 1.5s infinite;
 
-  @keyframes wiggle {
-    0% { transform: rotate(0deg); }
-    80% { transform: rotate(0deg); }
-    85% { transform: rotate(5deg); }
-    95% { transform: rotate(-5deg); }
-    100% { transform: rotate(0deg); }
+    @keyframes wiggle {
+      0% {
+        transform: rotate(0deg);
+      }
+      80% {
+        transform: rotate(0deg);
+      }
+      85% {
+        transform: rotate(5deg);
+      }
+      95% {
+        transform: rotate(-5deg);
+      }
+      100% {
+        transform: rotate(0deg);
+      }
+    }
   }
 `;
